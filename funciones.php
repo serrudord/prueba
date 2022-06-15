f99dcf6665352160c5d0fb25ae232b0e2086670c
franco <rolando@mycorreo.com.ar>
2022-06-15 13:36:39 -0300

    Se agrero el archivo funciones.php

---
+++ funciones.php
@@ -0,0 +1,15 @@
+<?php
+
+class prueba extends AnotherClass implements Interface
+{
+    
+    public function FunctionName(Type $var = null)
+    {
+        # code...
+        echo $var
+    }
+
+}
+
+
+?>
